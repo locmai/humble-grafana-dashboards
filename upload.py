1b2ef5605a3e11573ae2da673bfec3680817975e
Loc Mai <lmai@axon.com>
2021-08-26 01:41:35 +0700

    init screenshot wf

---
+++ upload.py
@@ -0,0 +1,19 @@
+import requests, json, os
+
+json_data = {}
+with open(f"_json/platform/argocd.json", 'w') as infile:
+    json_data = json.load(infile)
+
+gf_api_key = os.environ['GF_API_KEY']
+
+url = 'grafana.locmai.dev'
+
+headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': f"Bearer {gf_api_key}"
+}
+
+r = requests.post(url, json=json_data, headers=headers)
+
+print(r.status_code)

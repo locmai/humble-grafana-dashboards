eb66b1550ca45a44bc708091ac8b3fa7c1d102d8
Loc Mai <lmai@axon.com>
2021-08-26 01:54:01 +0700

    very secure

--- upload.py
+++ upload.py
@@ -1,19 +1,23 @@
 import requests, json, os
 
 json_data = {}
-with open(f"_json/platform/argocd.json", 'r') as infile:
+with open('_json/platform/argocd.json', 'r') as infile:
     json_data = json.load(infile)
 
 gf_api_key = os.environ['GF_API_KEY']
 
-url = 'https://grafana.locmai.dev'
+url = 'https://grafana.locmai.dev/api/dashboards/db'
+
+print(json_data)
 
 headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': f"Bearer {gf_api_key}"
+    'Authorization': f"Bearer {gf_api_key}",
 }
 
 r = requests.post(url, json=json_data, headers=headers)
 
 print(r.status_code)
+
+print(r.content)
